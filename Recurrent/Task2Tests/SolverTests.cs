41a4caec7916d35433537dab403695a27525a86b
George Pavlovych <pavlheo@gmail.com>
2017-01-14 15:12:10 +0200

    Test Category added

--- Recurrent/Task2Tests/SolverTests.cs
+++ Recurrent/Task2Tests/SolverTests.cs
@@ -8,24 +8,28 @@ namespace Task2Tests
         private const int CorrectAnswer = 55252;
 
         [TestMethod]
+        [TestCategory("Task2")]
         public void ExecuteObvious10To6()
         {
             Assert.AreEqual(SolverTest.CorrectAnswer, Task2.Solver.ExecuteObvious10To6());
         }
 
         [TestMethod]
+        [TestCategory("Task2")]
         public void Execute10To5()
         {
             Assert.AreEqual(SolverTest.CorrectAnswer, Task2.Solver.Execute10To5());
         }
 
         [TestMethod]
+        [TestCategory("Task2")]
         public void Execute10To3()
         {
             Assert.AreEqual(SolverTest.CorrectAnswer, Task2.Solver.Execute10To3());
         }
 
         [TestMethod]
+        [TestCategory("Task2")]
         public void Execute140()
         {
             Assert.AreEqual(SolverTest.CorrectAnswer, Task2.Solver.Execute140());
