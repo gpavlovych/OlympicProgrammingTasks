f176048b5e205263fa58ce9bf10eccc334d911ac
George Pavlovych <pavlheo@gmail.com>
2017-01-14 14:17:13 +0200

    Task 1 solved

---
+++ Task1/Solver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    /// <summary>
+    /// Solver for Task1: given by k, evaluate 1/1!+1/2!+..+1/k!
+    /// </summary>
+    public class Solver
+    {
+        public static double Execute(int k)
+        {
+            double s = 0.0;
+            double p = 1.0;
+
+            for (var i = 1; i <= k; i++)
+            {
+                p *= i;
+                s += 1.0/p;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ Task1Tests/SolverTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace Task1Tests
+{
+    [TestClass]
+    public class SolverTest
+    {
+        [TestMethod]
+        public void ExecuteNegative()
+        {
+            Assert.AreEqual(0, Solver.Execute(-1));
+        }
+
+        [TestMethod]
+        public void ExecuteZero()
+        {
+            Assert.AreEqual(0, Solver.Execute(0));
+        }
+
+        [TestMethod]
+        public void ExecuteOne()
+        {
+            Assert.AreEqual(1, Solver.Execute(1));
+        }
+
+        [TestMethod]
+        public void ExecuteTwo()
+        {
+            Assert.AreEqual(1.5, Solver.Execute(2));
+        }
+
+        [TestMethod]
+        public void ExecuteTwoThousands()
+        {
+            Assert.AreEqual(1.71828182845905, Solver.Execute(2000), 0.00000000000001);
+        }
+    }
+}
